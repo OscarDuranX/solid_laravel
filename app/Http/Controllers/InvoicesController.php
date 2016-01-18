324802b58faeb38ff28b3a862bbb1717bcc6a0b1
OscarDuranX <oscarduran@iesebre.com>
2016-01-18 17:58:30 +0100

    Controlador invoices fent masa responsabilitats!

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -2,27 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Invoices;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class InvoicesController extends Controller
 {
     public function index()
     {
-        if ( !Auth::check() ) {
-
-            return "Forbidden";
-        }
+//        if ( !Auth::check() ) {
+//
+//            return "Forbidden!";
+//        }
         
-        $invoices = $this->getAllInvoicesFormDatabase();
+        $database_invoices = $this->getAllInvoicesFormDatabase();
 
-        return view('invoices');
+        $invoices= $this->transform($database_invoices);
+
+        return view('invoices', compact('invoices'));
+
+//        $data['invoices'] = $formatted_invoices;
+//        return view('invoices');
     }
 
     public function getAllInvoicesFormDatabase()
     {
+        return Invoices::all();
         
+    }
+
+    private function transform($database_invoices)
+    {
+        return $database_invoices;
     }
 }
