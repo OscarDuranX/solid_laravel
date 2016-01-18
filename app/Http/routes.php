d9cfd6b6a8794b57f98121bebb4788063e6f088a
OscarDuranX <oscarduran@iesebre.com>
2016-01-18 17:16:18 +0100

    view invoices add

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,5 +27,10 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
+   Route::get('/invoices', 'InvoicesController@index');
+});
+
+
+Route::group(['middleware' => ['web']], function () {
     //
 });
