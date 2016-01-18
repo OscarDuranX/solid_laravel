b6841d46ff76fa973805897dc9d5f48898a6e2b9
OscarDuranX <oscarduran@iesebre.com>
2016-01-18 18:11:32 +0100

    invoiceRepositorye.php add

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Invoices;
 use Illuminate\Http\Request;
+use App\Repositories\InvoiceRepository;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -11,6 +12,18 @@ use Illuminate\Support\Facades\Auth;
 
 class InvoicesController extends Controller
 {
+
+    protected $invoiceRepo;
+
+    /**
+     * InvoicesController constructor.
+     * @param $invoiceRepo
+     */
+    public function __construct(InvoiceRepository $invoiceRepo)
+    {
+        $this->invoiceRepo = $invoiceRepo;
+    }
+
     public function index()
     {
 //        if ( !Auth::check() ) {
@@ -18,9 +31,8 @@ class InvoicesController extends Controller
 //            return "Forbidden!";
 //        }
 
-        $invoiceRepo = new InvoicesRepository();
 
-        $database_invoices = $invoiceRepo->getAllInvoicesFormDatabase();
+        $database_invoices = $this ->invoiceRepo->getAllInvoicesFormDatabase();
 
         $invoices= $this->transform($database_invoices);
 
