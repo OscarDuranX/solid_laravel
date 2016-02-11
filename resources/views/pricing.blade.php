f5eef90ef1efc8d52055c4d6622473d871d0b29d
OscarDuranX <oscarduran@iesebre.com>
2016-02-11 16:37:04 +0100

    model divs

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -4,22 +4,49 @@
     Pricing CSS!
 @endsection
 
+<style>
+    .taulapreus_row{
+        display: flex;
+    }
+
+    .llista{
+        flex: 1;
+        text-align: center;
+        margin: 0.5em;
+    }
+
+</style>
 
 @section('main-content')
-    <div class="container spark-screen">
-        <div class="row">
-           <div class="llista1">
-               <div class="llista1__header"></div>
+    <div class="container-fluid spark-screen">
+        <div class="taulapreus_row">
+
+           <div class="llista">
+               <div class="llista__header "> Cuota 1</div>
+               <div>
+                   <ul>
+                       <li></li>
+                       <li></li>
+                       <li></li>
+                       <li></li>
+                       <li></li>
+                   </ul>
+               </div>
+               <div>5€/mes</div>
+               <div>Adquirir</div>
 
 
            </div>
-           <div class="llista2">
+           <div class="llista">
+               <div class="llista__header"> Cuota 2</div>
 
            </div>
-           <div class="llista3">
+           <div class="llista">
+               <div class="llista__header"> Cuota 3</div>
 
            </div>
-           <div class="llista4">
+           <div class="llista">
+               <div class="llista__header"> Cuota 4</div>
 
            </div>
         </div>
